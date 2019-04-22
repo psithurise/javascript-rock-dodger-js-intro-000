227c620ec15c9dea7636f36be7c4a210afa5b7f4
Rose Stough <psithurise@programmer.net>
2019-04-22 22:44:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -132,13 +132,13 @@ function moveDodger(e) {
    * we've declared for you above.)
    * And be sure to use the functions declared below!
    */
-   if(e.which === LEFT_ARROW) {
+   if(e.which == LEFT_ARROW) {
      e.stopPropogation()
      e.preventDefault()
      moveDodgerLeft()
    }
 
-   else if(e.which === RIGHT_ARROW) {
+   else if(e.which == RIGHT_ARROW) {
      e.stopPropogation()
      e.preventDefault()
      moveDodgerRight()
