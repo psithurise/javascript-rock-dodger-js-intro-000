4e03d05973494ab66fbf7e9ee8744504e6576288
Rose Stough <psithurise@programmer.net>
2019-04-22 22:06:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -79,7 +79,7 @@ function createRock(x) {
      * If a rock collides with the DODGER,
      * we should call endGame()
      */
-     if(checkCollision(rock))
+     if(checkCollision(rock) == true)
       return endGame()
 
     /**
